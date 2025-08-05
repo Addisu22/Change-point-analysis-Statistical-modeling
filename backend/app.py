1c76d5de7687cbf96113a33aec1608ab4a9ccfcb
Addisu22 <liveaddisu@gmail.com>
2025-08-06 00:39:47 +0300

    Interactive Dashboard

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
 
 app = Flask(__name__)
 CORS(app, origins='*')
